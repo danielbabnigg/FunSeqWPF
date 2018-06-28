04bbddf72e40fe738c3a62bf0b5f156b2b94cb40
danielbabnigg <daniel@babnigg.com>
2018-06-28 11:19:23 -0500

    added tests

--- FunSeqWPF/Test02.xaml.cs
+++ FunSeqWPF/Test02.xaml.cs
@@ -23,5 +23,80 @@ namespace FunSeqWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("You left");
+        }
+
+        private void Button_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("You clicked twice");
+        }
+
+        private void Button_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            MessageBox.Show("You scrolled");
+        }
+
+        private void DatePicker_CalendarClosed(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You picked a date");
+        }
+
+        private void Slider_TouchLeave(object sender, TouchEventArgs e)
+        {
+            MessageBox.Show("You have a number");
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MessageBox.Show("Different number");
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("yay you got orange");
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("yay");
+        }
+
+        private void Ellipse_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("circles are good");
+        }
+
+        private void PasswordBox_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("enter a password (;");
+        }
+
+        private void PasswordBox_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            MessageBox.Show("kthxbye");
+        }
+
+        private void ScrollBar_Drop(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("cool");
+        }
+
+        private void Rectangle_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            MessageBox.Show("n  i  c  e");
+        }
+
+        private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
     }
 }
